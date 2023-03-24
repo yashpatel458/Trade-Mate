2cb077318342142f4133370294a21ef1082cc4cb
yashb.patel <yashb.softsages@gmail.com>
2023-03-24 13:24:10 +0530

    Updated

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -4,7 +4,7 @@ export const Features = (props) => {
   return (
     <div id="features" className="text-center">
       <div className="container">
-        <div className="col-md-10 col-md-offset-1 section-title">
+        <div className="section-title">
           <h2>Features</h2>
         </div>
         <div className="row">
@@ -12,7 +12,7 @@ export const Features = (props) => {
             ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
                   {" "}
-                  <i className={d.icon}></i>
+                  <img src={d.img}/>
                   <h3>{d.title}</h3>
                   <p>{d.text}</p>
                 </div>
